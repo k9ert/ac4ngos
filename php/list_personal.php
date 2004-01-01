c6b0fc43bd20df7eb1f3387ead5a7e4377fcff79
Kim Neunert <k9ert@users.noreply.github.com>
2004-01-01 16:56:41 +0000

    dummy

--- php/list_personal.php
+++ php/list_personal.php
@@ -7,10 +7,11 @@ require("security/secure.php");
 
 beginDocument($lCustomerProfile, $sess_user);
 
-$db = getDBConnection();
 
 $desig_array = get_designation_array();
 if (!isSet($orderby)) $orderby="EMP_ID3";
+
+$db = getDBConnection();
 $result = mysql_query("select * from PERSONAL order by $orderby", $db);
 checkMySQLError();
 beginPrettyTable("5", "PERSONAL");
